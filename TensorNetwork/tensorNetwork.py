f100d97c6862461f537ede1244318a9b98e44bdb
Adam Jermyn <adamjermyn@gmail.com>
2016-08-08 19:05:37 -0400

    Removed old code from tensorNetwork.

--- TensorNetwork/tensorNetwork.py
+++ TensorNetwork/tensorNetwork.py
@@ -1,14 +1,3 @@
-import numpy as np
-import networkx
-from collections import Counter
-from collections import defaultdict
-#import pymetis
-import itertools as it
-
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return it.chain.from_iterable(it.combinations(s, r) for r in range(len(s)+1))
 
 
 
