6debfa69a4e4d12ac1709573bef8938a975bb521
Adam Jermyn <adamjermyn@gmail.com>
2016-08-12 15:21:58 -0400

    Switched to iterative SVD.

--- TensorNetwork/compress.py
+++ TensorNetwork/compress.py
@@ -2,14 +2,39 @@ from tensor import Tensor
 import numpy as np
 from scipy.sparse.linalg import svds
 
-def bigSVD(matrix):
-	err = 1
-	k = 2
-	while err > 1e-4:
-		u, s, v = svds(matrix, k=k)
-		err = np.sum(np.abs(matrix - np.einsum('ij,j,jk->ik',u,s,v)))/np.sum(np.abs(matrix))
-		k += 4
-	print 'k=',k-4,np.abs(s)/np.sum(s)
+def cutBond(u, v, n1, n2, ind1, ind2, link, sh1m, sh2m):
+	u = np.reshape(u,sh1m)
+	v = np.reshape(v,sh2m)
+
+	t1m = Tensor(u.shape,u)
+	t2m = Tensor(v.shape,v)
+
+	n1m = n1.modify(t1m, delBuckets=[ind1])
+	n2m = n2.modify(t2m, delBuckets=[ind2])
+
+	# These four lines make me a bit nervous. In particular,
+	# there should be a more elegant way to say that this link is not top-level without
+	# incurring infinite recursion in the inheritance. Not that I mind, but doing this
+	# could introduce obnoxious edge cases if I'm not careful. It might be better to have
+	# an explicit list of no-parent-yet-top cases and just exclude them from top-level
+	# things... but that might be slow. Anyway, I also can't quite figure
+	# out whether or not the update call is necessary.
+	link.setCompressed()
+	link.setParent(link) # So it isn't considered top-level
+	link.network().deregisterLinkTop(link) # So it's removed from the top-level.
+	link.update() # So it's up to date.
+
+	return link, n1m, n2m
+
+def bigSVD(matrix, k):
+	if k == 0:
+		return np.linalg.svd(matrix)
+	u, s, v = svds(matrix, k=k)
+	inds = np.argsort(s)
+	inds = inds[::-1]
+	u = u[:,inds]
+	s = s[inds]
+	v = v[inds,:]
 	return u, s, v
 
 def compress(link, eps=1e-4):
@@ -22,22 +47,25 @@ def compress(link, eps=1e-4):
 	arr1 = t1.array()
 	arr2 = t2.array()
 
-	ind1 = n1.bucketIndex(link.bucket1())
-	ind2 = n2.bucketIndex(link.bucket2())
-
-	cont = t1.contract(ind1,t2,ind2)
-	arrN = cont.array()
-
 	sh1 = list(arr1.shape)
 	sh2 = list(arr2.shape)
+
+	ind1 = n1.bucketIndex(link.bucket1())
+	ind2 = n2.bucketIndex(link.bucket2())
 
 	sh1m = sh1[:ind1] + sh1[ind1+1:]
 	sh2m = sh2[:ind2] + sh2[ind2+1:]
 
+	if arr1.shape[ind1] == 1: # Means we just cut the bond
+		return cutBond(np.copy(arr1), np.copy(arr2), n1, n2, ind1, ind2, link, sh1m, sh2m)
+
+	cont = t1.contract(ind1,t2,ind2)
+	arrN = cont.array()
+
 	arrN = np.reshape(arrN,(np.product(sh1m),np.product(sh2m)))
 
-#	u, lam, v = bigSVD(arrN)
-	u, lam, v = np.linalg.svd(arrN,full_matrices=0)
+	u, lam, v = bigSVD(arrN, min(sh1[ind1], min(arrN.shape)-1))
+#	u, lam, v = np.linalg.svd(arrN,full_matrices=0)
 
 	p = lam**2
 	p /= np.sum(p)
@@ -81,27 +109,6 @@ def compress(link, eps=1e-4):
 
 			newLink = n1m.addLink(n2m, ind1, ind2, compressed=True, children=[link])
 		else:	# Means we're just cutting the bond
-			u = np.reshape(u,sh1m)
-			v = np.reshape(v,sh2m)
-
-			t1m = Tensor(u.shape,u)
-			t2m = Tensor(v.shape,v)
-
-			n1m = n1.modify(t1m, delBuckets=[ind1])
-			n2m = n2.modify(t2m, delBuckets=[ind2])
-
-			# These four lines make me a bit nervous. In particular,
-			# there should be a more elegant way to say that this link is not top-level without
-			# incurring infinite recursion in the inheritance. Not that I mind, but doing this
-			# could introduce obnoxious edge cases if I'm not careful. It might be better to have
-			# an explicit list of no-parent-yet-top cases and just exclude them from top-level
-			# things... but that might be slow. Anyway, I also can't quite figure
-			# out whether or not the update call is necessary.
-			link.setCompressed()
-			link.setParent(link) # So it isn't considered top-level
-			link.network().deregisterLinkTop(link) # So it's removed from the top-level.
-			link.update() # So it's up to date.
-
-			newLink = link
+			return cutBond(u, v, n1, n2, ind1, ind2, link, sh1m, sh2m)
 
 	return newLink, n1m, n2m
