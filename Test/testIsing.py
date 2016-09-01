dde26e0af99be0a0962aa96c6c0c60b0c923f4e0
Adam Jermyn <adamjermyn@gmail.com>
2016-09-01 12:10:56 +0100

    Added random Ising model to test suite.

--- Test/testIsing.py
+++ Test/testIsing.py
@@ -2,8 +2,14 @@ import sys
 sys.path.append('../TensorNetwork/')
 sys.path.append('../Models/')
 import numpy as np
-from isingModel import IsingModel2D, exactIsing2D, IsingModel3D
+from isingModel import IsingModel2D, exactIsing2D, IsingModel3D, RandomIsingModel2D
 import cProfile
+
+#######
+# 2D ##
+#######
+
+print('Testing 2D Regular Ising Model - J sweep, h=0:')
 
 nX = 10
 nY = 10
@@ -14,7 +20,29 @@ for j in np.linspace(-1,1,num=10):
 	arr, logS, _ = network.topLevelRepresentation()
 	q = np.log(arr) + logS
 	q /= (nX*nY)
-	print(j,q,exactIsing2D(j))
+	print('J=',j,'lnZ (calculated)=',q,'lnZ (exact)=',exactIsing2D(j))
+
+#######
+# 2D ##
+#######
+
+print('Testing 2D Random Ising Model:')
+
+nX = 10
+nY = 10
+
+network, lattice = RandomIsingModel2D(nX, nY)
+network.contract(mergeL=True, compressL=True, eps=1e-4)
+arr, logS, _ = network.topLevelRepresentation()
+q = np.log(arr) + logS
+q /= (nX*nY)
+print('lnZ (calculated)=',q)
+
+#######
+# 3D ##
+#######
+
+print('Testing 3D Regular Ising Model - J=0.5,h=0.5:')
 
 nX = 8
 nY = 8
@@ -24,4 +52,4 @@ network, lattice=  IsingModel3D(nX, nY, nZ, 0.5, 0.5)
 network.contract(mergeL=True, compressL=True, eps=1e-4)
 arr, logS, _ = network.topLevelRepresentation()
 q = np.log(arr) + logS
-print(q)
+print('lnZ (calculated)=',q)
