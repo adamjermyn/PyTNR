edf0cf0d96f06c6b73a465e68f57548d4b4d9c63
Adam Jermyn <adamjermyn@gmail.com>
2016-09-01 11:07:00 +0100

    Added Model folder and isingModel2D model file.

---
+++ Models/isingModel2D.py
@@ -0,0 +1,59 @@
+import sys 
+sys.path.append('../TensorNetwork/') 
+from network import Network 
+from latticeNode import latticeNode 
+import numpy as np 
+from scipy.integrate import quad 
+import cProfile 
+ 
+def IsingSolve(nX, nY, h, J): 
+	network = Network() 
+ 
+	# Place to store the tensors 
+	lattice = [[] for i in range(nX)] 
+	onSite = [[] for i in range(nX)] 
+	bondV = [[] for i in range(nX)] 
+	bondH = [[] for i in range(nY)] 
+ 
+	# Each lattice site has seven indices of width five, and returns zero if they are unequal and one otherwise. 
+	for i in range(nX): 
+		for j in range(nY): 
+			lattice[i].append(latticeNode(2,network)) 
+ 
+	# Each on-site term has one index of width two, and returns exp(-h) or exp(h) for 0 or 1 respectively. 
+	for i in range(nX): 
+		for j in range(nY): 
+			arr = np.zeros((2)) 
+			arr[0] = np.exp(-h) 
+			arr[1] = np.exp(h) 
+			onSite[i].append(network.addNodeFromArray(arr)) 
+			lattice[i][j].addLink(onSite[i][j],0) 
+ 
+	# Each bond term has two indices of width two and returns exp(-J*(1+delta(index0,index1))/2). 
+	for i in range(nX): 
+		for j in range(nY): 
+			arr = np.zeros((2,2)) 
+			arr[0][0] = np.exp(-J) 
+			arr[1][1] = np.exp(-J) 
+			arr[0][1] = np.exp(J) 
+			arr[1][0] = np.exp(J) 
+			bondV[i].append(network.addNodeFromArray(np.copy(arr))) 
+			bondH[i].append(network.addNodeFromArray(np.copy(arr))) 
+ 
+	# Attach bond terms 
+	for i in range(nX): 
+		for j in range(nY): 
+			lattice[i][j].addLink(bondV[i][j],0) 
+			lattice[i][j].addLink(bondV[i][(j+1)%nY],1) 
+			lattice[i][j].addLink(bondH[i][j],0) 
+			lattice[i][j].addLink(bondH[(i+1)%nX][j],1) 
+ 
+	return network, lattice 
+ 
+def exactIsing(J): 
+	k = 1/np.sinh(2*J)**2 
+	def f(x): 
+		return np.log(np.cosh(2*J)**2 + (1/k)*np.sqrt(1+k**2-2*k*np.cos(2*x))) 
+	inte = quad(f,0,np.pi)[0] 
+ 
+	return np.log(2)/2 + (1/(2*np.pi))*inte 
