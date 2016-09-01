adad14ffb0e488a320922eff50d611f1aed62ba7
Adam Jermyn <adamjermyn@gmail.com>
2016-09-01 12:05:47 +0100

    Added 3D test to Ising test.

--- Test/testIsing.py
+++ Test/testIsing.py
@@ -2,16 +2,26 @@ import sys
 sys.path.append('../TensorNetwork/')
 sys.path.append('../Models/')
 import numpy as np
-from isingModel2D import IsingModel, exactIsing
+from isingModel import IsingModel2D, exactIsing2D, IsingModel3D
 import cProfile
 
 nX = 10
 nY = 10
 
 for j in np.linspace(-1,1,num=10):
-	network, lattice = IsingModel(nX, nY, 0, j)
+	network, lattice = IsingModel2D(nX, nY, 0, j)
 	network.contract(mergeL=True, compressL=True, eps=1e-4)
 	arr, logS, _ = network.topLevelRepresentation()
 	q = np.log(arr) + logS
 	q /= (nX*nY)
-	print(j,q,exactIsing(j))
+	print(j,q,exactIsing2D(j))
+
+nX = 10
+nY = 10
+nZ = 10
+
+network, lattice=  IsingModel3D(nX, nY, nZ, 0.5, 0.5)
+network.contract(mergeL=True, compressL=True, eps=1e-4)
+arr, logS, _ = network.topLevelRepresentation()
+q = np.log(arr) + logS
+print(q)
