a752fb9072676a07842c17aa208a1d850af4d5fc
Adam Jermyn <adamjermyn@gmail.com>
2016-09-07 13:06:40 +0100

    Added optimizer test.

---
+++ Test/testOptimizer.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../TensorNetwork/')
+sys.path.append('../Models/')
+import numpy as np
+from isingModel import IsingModel2D, exactIsing2D, IsingModel3D, RandomIsingModel2D
+import cProfile
+
+nX = 10
+nY = 10
+
+network, lattice = IsingModel2D(nX, nY, 0.1, 0.5)
+network.contract(mergeL=True, compressL=True, eps=1e-4)
+network.optimize(mergeL=True, compressL=True, eps=1e-4)
+
+arr, logS, _ = network.topLevelRepresentation()
+q = np.log(arr) + logS
+q /= (nX*nY)
+print('J=',j,'lnZ (calculated)=',q,'lnZ (exact)=',exactIsing2D(j))
