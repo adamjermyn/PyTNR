03f3576216d848831284ec7f90773c90cfdf99ed
Adam Jermyn <adamjermyn@gmail.com>
2016-10-22 14:25:23 +0100

    Made buckets hashable.

--- Tensor/bucket.py
+++ Tensor/bucket.py
@@ -1,9 +1,16 @@
+import itertools
 
 class Bucket:
+
+	newid = itertools.count().next
 
 	def __init__(self):
 		self.node = None
 		self.link = None
+		self.id = Bucket.newid()
+
+	def __hash__(self):
+		return self.id
 
 	@property
 	def otherBucket(self):
