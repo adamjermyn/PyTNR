bbb8d44d360eaeaa9b4455ca38bdcc41cd51881a
Adam Jermyn <adamjermyn@gmail.com>
2016-12-03 10:57:38 +0000

    Refactored ising test.

--- Tensor/isintTest.py
+++ Tensor/isintTest.py
@@ -1,67 +1,19 @@
-from bucket import Bucket
-from link import Link
-from node import Node
-from treeTensor import TreeTensor as TT
-from arrayTensor import ArrayTensor as AT
-from treeNetwork import TreeNetwork as TN
-from network import Network
+import sys 
+sys.path.append('../Models/') 
+sys.path.append('../Contractors/') 
 import numpy as np
 from compress import compressLink
+from isingModel import IsingModel2D
+from mergeContractor import mergeContractor
 
-nX = 3
-nY = 3
-nZ = 3
+nX = 10
+nY = 10
 accuracy = 1e-4
+h = 0.1
+J = 0.45
 
-nodes = [[[] for _ in range(nY)] for _ in range(nX)]
+n = IsingModel2D(nX, nY, h, J)
 
-n = Network()
-
-x = np.exp(np.random.randn(2,2,2,2,2,2))
+mergeContractor(n, accuracy, verbose=1)
 
 
-for i in range(nX):
-	for j in range(nY):
-		for k in range(nZ):
-			t = AT(x)
-			tf = TT(accuracy)
-			tf.addTensor(t)
-			nodes[i][j].append(Node(tf, Buckets=[Bucket() for _ in range(len(x.shape))]))
-
-for i in range(nX):
-	for j in range(nY):
-		for k in range(nZ):
-			l = Link(nodes[i][j][k].buckets[0], nodes[(i+1)%nX][j][k].buckets[3])
-			l = Link(nodes[i][j][k].buckets[1], nodes[i][(j+1)%nY][k].buckets[4])
-			l = Link(nodes[i][j][k].buckets[2], nodes[i][j][(k+1)%nZ].buckets[5])
-
-for i in range(nX):
-	for j in range(nY):
-		for k in range(nZ):
-			n.addNode(nodes[i][j][k])
-
-while len(n.nodes) > 1:
-	smallest = [1e20,None,None]
-	for nn in n.nodes:
-		for nnn in nn.connectedNodes:
-			length = nn.linksConnecting(nnn)[0].bucket1.size
-			metric = nn.tensor.size*nnn.tensor.size/length**2
-			commonNodes = set(nn.connectedNodes).intersection(nnn.connectedNodes)
-			metric *= 0.75**len(commonNodes)
-			metric = metric - nn.tensor.size - nnn.tensor.size
-			if metric < smallest[0]:
-				smallest[0] = metric
-				smallest[1] = nn
-				smallest[2] = nnn
-	n1 = smallest[1]
-	n2 = smallest[2]
-	n3 = n.mergeNodes(n1, n2)
-	n.mergeLinks(n3, accuracy=accuracy)
-	n3.tensor.optimize()
-
-	for nn in n.nodes:
-		print(nn.tensor.shape, nn.tensor.size, 1.0*nn.tensor.size/np.product(nn.tensor.shape))
-
-	print('-------------------------------')
-	print('-------',smallest[0],len(n3.connectedNodes),len(n.nodes),'-------')
-	print('-------------------------------')
