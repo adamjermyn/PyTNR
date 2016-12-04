5f69ea59686af5871e059143c31237a5a9e6ec63
Adam Jermyn <adamjermyn@gmail.com>
2016-12-04 10:16:45 +0000

    Added more tests to test script.

--- Tensor/test.py
+++ Tensor/test.py
@@ -6,14 +6,25 @@ from treeNetwork import TreeNetwork as TN
 import numpy as np
 
 
-x = np.random.randn(4,4,4,4,4)
+x = np.random.randn(2,3,4,5,6)
+t = AT(x)
+tf = TT(1e-5)
+tf.addTensor(t)
+
+print(tf.shape)
+print(x - tf.array)
+print(tf.network)
+
+print('-------')
+
+x = np.random.randn(2,3,2,3,2)
 t = AT(x)
 tf = TT(1e-5)
 tf.addTensor(t)
 
 print(tf)
 
-xx = np.random.randn(4,4,4,4,4)
+xx = np.random.randn(2,2,2,3,2)
 t = AT(xx)
 tf2 = TT(1e-5)
 tf2.addTensor(t)
@@ -24,11 +35,6 @@ print('-------')
 
 tf3 = tf.contract([0,1,2],tf2,[2,3,4])
 
-print(tf3)
+print('h',tf3)
 
-print(tf3.network.array())
-print(np.einsum('ijrwp,sqijr->wspq',x,xx))
-print(np.sum(tf3.network.array()**2))
-print(np.sum(np.einsum('ijrwp,sqijr->wspq',x,xx)**2))
-
-tf4 = tf3.trace(0,1)
+print(np.einsum('ijrwp,sqijr->wpsq',x,xx) - tf3.array)
