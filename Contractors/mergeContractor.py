9ceb04239175271b584825d7b48251f190b05518
Adam Jermyn <adamjermyn@gmail.com>
2016-12-05 16:35:47 +0000

    Added a second heuristic for merging.

--- Contractors/mergeContractor.py
+++ Contractors/mergeContractor.py
@@ -14,9 +14,35 @@ def entropyHeuristic(n):
 				smallest[0] = metric
 				smallest[1] = nn
 				smallest[2] = nnn
-	n1 = smallest[1]
-	n2 = smallest[2]
-	return n1, n2	
+	return smallest
+
+def loopHeuristic(n):
+	'''
+	This method estimates the contraction in a network which maximizes the size of the loop which
+	is eliminated in the process.
+	'''
+	biggest = [-1, None, None]
+	for nn in n.nodes:
+		for nnn in nn.connectedNodes:
+			indices = nn.indicesConnecting(nnn)
+			length = 1
+			if not hasattr(nn.tensor, 'network') or not hasattr(nnn.tensor, 'network'):
+				length = 100
+			else:
+				for i in range(len(indices[0])):
+					for j in range(len(indices[0])):
+						if i > j:
+							length1 = nn.tensor.distBetween(indices[0][i],indices[0][j])
+							if length1 > length:
+								length = length1
+							length1 = nnn.tensor.distBetween(indices[1][i],indices[1][j])
+							if length1 > length:
+								length = length1
+			if length > biggest[0]:
+				biggest[0] = length
+				biggest[1] = nn
+				biggest[2] = nnn
+	return biggest
 
 def mergeContractor(n, accuracy, optimize=True, merge=True, verbose=0):
 	'''
@@ -33,12 +59,12 @@ def mergeContractor(n, accuracy, optimize=True, merge=True, verbose=0):
 		2 - Node enumeration
 	'''
 	while len(n.nodes) > 1:
-		n1, n2 = entropyHeuristic(n)
+		q, n1, n2 = entropyHeuristic(n)
 		n3 = n.mergeNodes(n1, n2)
 		if merge:
 			n.mergeLinks(n3, accuracy=accuracy)
 		if optimize:
-			n3.tensor.optimize()
+			n3.tensor.optimize(verbose=verbose)
 
 		if verbose >= 2:
 			for nn in n.nodes:
@@ -48,6 +74,10 @@ def mergeContractor(n, accuracy, optimize=True, merge=True, verbose=0):
 					print(nn.tensor.shape, nn.tensor.size)
 
 		if verbose >= 1:
-			print('-------',len(n3.connectedNodes),len(n.nodes),'-------')
+			counter = 0
+			for nn in n.nodes:
+				if hasattr(nn.tensor, 'network'):
+					counter += 1
+			print('-------',len(n3.connectedNodes),q,counter,len(n.nodes),'-------')
 
 	return n
