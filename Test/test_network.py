01577d36e680195b83c262757c2b4b0dba4de468
Adam Jermyn <adamjermyn@gmail.com>
2016-12-11 18:51:49 +0000

    Added more tests to Network.

--- Test/test_network.py
+++ Test/test_network.py
@@ -51,34 +51,119 @@ def test_init():
 	assert len(net.optimizedLinks) == 0
 
 def test_mergeNode():
-	net = Network()
+	for i in range(5):
+		net = Network()
 
-	x = np.random.randn(2,3,3)
-	xt = ArrayTensor(x)
-	n1 = Node(xt)
-	xt = ArrayTensor(x)
-	n2 = Node(xt)
+		x = np.random.randn(2,3,3)
+		xt = ArrayTensor(x)
+		n1 = Node(xt)
+		xt = ArrayTensor(x)
+		n2 = Node(xt)
 
-	net.addNode(n1)
-	Link(n1.buckets[0], n2.buckets[0])
-	net.addNode(n2)
+		net.addNode(n1)
+		Link(n1.buckets[0], n2.buckets[0])
+		net.addNode(n2)
 
-	net.mergeNodes(n1, n2)
+		net.mergeNodes(n1, n2)
 
-	assert len(net.nodes) == 1
-	assert len(net.buckets) == 4
-	assert len(net.internalBuckets) == 0
-	assert len(net.externalBuckets) == 4
-	assert len(net.optimizedLinks) == 0
+		assert len(net.nodes) == 1
+		assert len(net.buckets) == 4
+		assert len(net.internalBuckets) == 0
+		assert len(net.externalBuckets) == 4
+		assert len(net.optimizedLinks) == 0
 
-	arr, bdict = net.array
-	assert arr.shape == (3,3,3,3)
-	for b in net.buckets:
-		assert b.id in bdict
-	for b1 in net.buckets:
-		for b2 in net.buckets:
-			if b1.id < b2.id:
-				assert bdict[b1.id] < bdict[b2.id]
+		arr, bdict = net.array
+		assert arr.shape == (3,3,3,3)
+		for b in net.buckets:
+			assert b.id in bdict
+		for b1 in net.buckets:
+			for b2 in net.buckets:
+				if b1.id < b2.id:
+					assert bdict[b1.id] < bdict[b2.id]
 
-	assert np.sum((arr - np.einsum('ijk,ilm->jklm',x,x))**2) < epsilon
+		assert np.sum((arr - np.einsum('ijk,ilm->jklm',x,x))**2) < epsilon
 
+	for i in range(5):
+		net = Network()
+
+		x = np.random.randn(2,2,2,2,2)
+		xt = TreeTensor(accuracy=epsilon)
+		xt.addTensor(ArrayTensor(x))
+		n1 = Node(xt)
+		y = np.random.randn(2,2,2,2,2)
+		xt = TreeTensor(accuracy=epsilon)
+		xt.addTensor(ArrayTensor(y))
+		n2 = Node(xt)
+
+		net.addNode(n1)
+		Link(n1.buckets[0], n2.buckets[0])
+		net.addNode(n2)
+
+		net.mergeNodes(n1, n2)
+
+		assert len(net.nodes) == 1
+		assert len(net.buckets) == 8
+		assert len(net.internalBuckets) == 0
+		assert len(net.externalBuckets) == 8
+		assert len(net.optimizedLinks) == 0
+
+		arr, bdict = net.array
+		assert arr.shape == (2,2,2,2,2,2,2,2)
+		for b in net.buckets:
+			assert b.id in bdict
+		for b1 in net.buckets:
+			for b2 in net.buckets:
+				if b1.id < b2.id:
+					assert bdict[b1.id] < bdict[b2.id]
+
+		assert np.sum((arr - np.einsum('ijklm,iqwer->jklmqwer',x,y))**2) < epsilon
+
+def test_mergeLinks():
+	for i in range(5):
+		net = Network()
+
+		x = np.random.randn(2,2,3,3)
+		xt = ArrayTensor(x)
+		n1 = Node(xt)
+		xt = ArrayTensor(x)
+		n2 = Node(xt)
+
+		net.addNode(n1)
+		Link(n1.buckets[0], n2.buckets[0])
+		Link(n1.buckets[1], n2.buckets[1])
+		net.addNode(n2)
+
+		arr1, bdict1 = net.array
+
+		net.mergeLinks(n1, accuracy=epsilon)
+
+		arr2, bdict2 = net.array
+
+		assert np.sum((arr1 - arr2)**2) < epsilon
+		assert np.sum((arr1 - arr2)**2) < epsilon
+
+	for i in range(5):
+		net = Network()
+
+		x = np.random.randn(2,2,2,2,3,3)
+		xt = TreeTensor(accuracy=epsilon)
+		xt.addTensor(ArrayTensor(x))
+		n1 = Node(xt)
+		x = np.random.randn(2,2,2,2,3,3)
+		xt = TreeTensor(accuracy=epsilon)
+		xt.addTensor(ArrayTensor(x))
+		n2 = Node(xt)
+
+		net.addNode(n1)
+		Link(n1.buckets[0], n2.buckets[0])
+		Link(n1.buckets[1], n2.buckets[1])
+		net.addNode(n2)
+
+		arr1, bdict1 = net.array
+
+		net.mergeLinks(n1, accuracy=epsilon)
+
+		arr2, bdict2 = net.array
+
+		assert np.sum((arr1 - arr2)**2) < epsilon
+		assert np.sum((arr1 - arr2)**2) < epsilon
