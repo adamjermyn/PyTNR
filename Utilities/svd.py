fdea1bc784e4abbc0be691fbb40fc6b0bcaa0206
Adam Jermyn <adamjermyn@gmail.com>
2016-12-21 20:43:00 +0000

    Added method for extracing singular values without vectors.
    Also added methods for iterative singular value extraction with threshold comparison.

--- Utilities/svd.py
+++ Utilities/svd.py
@@ -50,7 +50,24 @@ def bigSVD(matrix, bondDimension):
 	v = v[inds, :]
 	return u, s, v
 
-def generalSVD(matrix, bondDimension=np.inf, optimizerMatrix=None, arr1=None, arr2=None):
+def bigSVDvals(matrix, bondDimension):
+	'''
+	This is a helper method for wrapping the iterative SVD method scipy provides.
+	It takes as input a matrix and an integer bond dimension which corresponds
+	to the bond which was contracted to form this matrix. It returns the SVD
+	with the singular values sorted in descending order (which the iterative
+	solver does not on its own guarantee).
+	'''
+	s = svds(matrix, k=bondDimension, return_singular_vectors=False)
+	inds = np.argsort(s)
+	inds = inds[::-1]
+	s = s[inds]
+	return s
+
+
+
+
+def generalSVD(matrix, bondDimension=np.inf, optimizerMatrix=None, arr1=None, arr2=None, precision=1e-5):
 	'''
 	This is a helper method for SVD calculations.
 
@@ -93,6 +110,27 @@ def generalSVD(matrix, bondDimension=np.inf, optimizerMatrix=None, arr1=None, ar
 		up, lam, vp = np.linalg.svd(arr3, full_matrices=0)
 		u = np.dot(u1, up)
 		v = np.dot(vp, v2)
+	elif precision is not None and bondDimension is np.inf and min(matrix.shape) > 4:
+		# Matches Frobenius norm
+		norm = np.linalg.norm(matrix)**2
+		err = norm
+
+		bondDimension = 2
+
+		while err > precision:
+			print('Matrix:',matrix.shape)
+			print('BD:',bondDimension)
+			bondDimension *= 2
+			if bondDimension > min(matrix.shape) - 1:
+				u, lam, v = np.linalg.svd(matrix, full_matrices=0)
+				err = 0
+			else:
+				u, lam, v = bigSVD(matrix, bondDimension)
+				err = abs(norm - np.sum(lam**2))
+		if err == 0:
+			print('BD:',min(matrix.shape),min(matrix.shape))
+		else:
+			print('BD:',bondDimension,min(matrix.shape))
 	elif optimizerMatrix is None:
 		if bondDimension > 0 and bondDimension < matrix.shape[0] and bondDimension < matrix.shape[1]:
 			# Required so sparse bond is properly represented
@@ -135,12 +173,86 @@ def generalSVD(matrix, bondDimension=np.inf, optimizerMatrix=None, arr1=None, ar
 
 	return u, lam, v, p, cp
 
+def generalSVDvals(matrix, bondDimension=np.inf, optimizerMatrix=None, precision=1e-5):
+	'''
+	This is a helper method for SVD calculations.
+
+	In the case where the matrix of interest was formed as a product A*B where
+	A.shape[1] == B.shape[0] << A.shape[0], B.shape[1], the SVD is mathematically
+	guaranteed to return at most A.shape[1] nonzero singular values, and so
+	we ought to use an iterative solver rather than the full solver.
+
+	In the remaining cases the iterative solve will fail because it cannot retrieve
+	all singular values of a matrix (it can retrieve at most one fewer). This is
+	just an implementation detail, and only arises in rare cases, so we just revert
+	to the full SVD solve.
+
+	If an optimizerArray is provided, we perform the procedure outlined in Eq7-13
+	in "Second Renormalization of Tensor-Network States" by Xie et. al. (arXiv:0809.0182v3).
+
+	This method accepts as input:
+		matrix 			-	The matrix to SVD.
+		bondDimension	-	The bond dimension used to form the matrix (i.e. A.shape[1]).
+		optimizerTensor	-	The 'environment' array to optimize against.
+
+	This method returns:
+		lam	-	An array of the singular values
+		p 	-	An array whose entries reflect the portion of the total weight in each
+				singular value.
+		cp 	-	An array whose entries reflect the cumulative portion of the total weight
+				associated with all singular values past a given index.
+
+	As a result of the above definitions, p and cp are both sorted in descending order.
+	'''
+	if precision is not None and bondDimension is np.inf and min(matrix.shape) > 4:
+		# Matches Frobenius norm
+		norm = np.linalg.norm(matrix)**2
+		err = norm
+
+		bondDimension = 2
+
+		while err > precision:
+			print('Matrix:',matrix.shape)
+			print('BD:',bondDimension)
+			bondDimension *= 2
+			if bondDimension > min(matrix.shape) - 1:
+				lam = np.linalg.svd(matrix, full_matrices=0, compute_uv=False)
+				err = 0
+			else:
+				lam = bigSVDvals(matrix, bondDimension)
+				err = abs(norm - np.sum(lam**2))
+		if err == 0:
+			print('BD:',min(matrix.shape),min(matrix.shape))
+		else:
+			print('BD:',bondDimension,min(matrix.shape))
+	elif optimizerMatrix is None:
+		if bondDimension > 0 and bondDimension < matrix.shape[0] and bondDimension < matrix.shape[1]:
+			# Required so sparse bond is properly represented
+			lam = bigSVDvals(matrix, bondDimension)
+		else:
+			try:
+				lam = np.linalg.svd(matrix, full_matrices=0, compute_uv=False)
+			except:
+				print('Erm.... SVD not converged!')
+				print(matrix)
+				print(np.sum(matrix**2))
+				print(matrix.shape)
+				print(np.max(matrix), np.min(matrix))
+				np.savetxt('error',matrix)
+				exit()
+
+	p = lam**2
+	p /= np.sum(p)
+	cp = np.cumsum(p[::-1])
+
+	return lam, p, cp
+
 def entropy(array, indices):
 	arr = permuteIndices(array, indices)
 	sh = arr.shape[:len(indices)]
 	s = np.product(sh)
 	arr = np.reshape(arr, (s,-1))
-	u, lam, v, p, cp = generalSVD(arr)
+	lam, p, cp = generalSVDvals(arr, precision=1e-5)
 	s = -np.sum(p*np.log(p))
 	return s
 
