8b28f8fe5d4337737ddd6e1bacfa0c7933ef67ae
Adam Jermyn <adamjermyn@gmail.com>
2017-02-08 09:58:28 +0000

    Added more merger heuristics.

--- Contractors/mergeContractor.py
+++ Contractors/mergeContractor.py
@@ -16,6 +16,64 @@ def entropyHeuristic(n):
 				smallest[2] = nnn
 	return smallest
 
+def mergeHeuristic(n):
+	'''
+	This method estimates the contraction in a network n which maximizes the number of merged links.
+	'''
+	biggest = [-1e20,None,None]
+	for nn in n.nodes:
+		for nnn in nn.connectedNodes:
+			if nnn.tensor.rank <= 2 or nn.tensor.rank <= 2:
+				return [1e20, nn, nnn]
+			commonNodes = set(nn.connectedNodes).intersection(nnn.connectedNodes)
+			metric = len(commonNodes)
+			if metric > biggest[0]:
+				biggest = [metric, nn, nnn]
+	return biggest
+
+def smallLoopHeuristic(n):
+	'''
+	This method estimates the contraction in a network which minimizes the size of the loop which
+	is eliminated in the process while penalizing rank increases.
+
+	In particular, this heuristic handles all rank <= 2 objects first as well as all objects
+	which are not yet tree tensors.
+
+	It then weights subsequent contractions as
+		(length of biggest loop) + (tensor 1 rank) + (tensor 2 rank) - (# common nodes)
+	and picks the smallest weight. This means that it prioritizes handling smaller tensors,
+	handling smaller loops, and handling pairs of tensors which share many nodes.
+	'''
+	smallest = [1e20, None, None]
+	for nn in n.nodes:
+		for nnn in nn.connectedNodes:
+			indices = nn.indicesConnecting(nnn)
+			length = 1
+			if not hasattr(nn.tensor, 'network') or not hasattr(nnn.tensor, 'network'):
+				length = -100
+			elif nn.tensor.rank <= 2 or nnn.tensor.rank <= 2:
+				length = -100
+			else:
+				for i in range(len(indices[0])):
+					for j in range(len(indices[0])):
+						if i > j:
+							length1 = nn.tensor.distBetween(indices[0][i],indices[0][j])
+							if length1 > length:
+								length = length1
+							length1 = nnn.tensor.distBetween(indices[1][i],indices[1][j])
+							if length1 > length:
+								length = length1
+				commonNodes = set(nn.connectedNodes).intersection(nnn.connectedNodes)
+				length -= len(commonNodes)
+				length += nn.tensor.rank
+				length += nnn.tensor.rank
+			metric = length
+			if metric < smallest[0]:
+				smallest[0] = metric
+				smallest[1] = nn
+				smallest[2] = nnn
+	return smallest
+
 def loopHeuristic(n):
 	'''
 	This method estimates the contraction in a network which maximizes the size of the loop which
@@ -84,9 +142,10 @@ def mergeContractor(n, accuracy, optimize=True, merge=True, verbose=0):
 	'''
 #	node = next(iter(n.nodes))
 	while len(n.nodes) > 1:
-#		q, n1, n2 = loopHeuristic(n)
-		q, n1, n2 = entropyHeuristic(n)
+		q, n1, n2 = smallLoopHeuristic(n)
+#		q, n1, n2 = entropyHeuristic(n)
 #		q, n1, n2 = oneLoopHeuristic(n, node)
+#		q, n1, n2 = mergeHeuristic(n)
 
 		n3 = n.mergeNodes(n1, n2)
 
