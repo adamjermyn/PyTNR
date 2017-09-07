a4208cb108a1e6bc7c951695768fb57c3ae2758a
Adam Jermyn <adamjermyn@gmail.com>
2017-09-07 14:46:32 +0100

    Fixed unknown bug in logger.

--- Utilities/logger.py
+++ Utilities/logger.py
@@ -1,7 +1,14 @@
 import logging
+import sys
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+handlers = [stdout_handler]
+
+logging.basicConfig(handlers=handlers)
 
 def makeLogger(name, level):
 	logger = logging.getLogger(name)
+
 	if level == 'info':
 		logger.setLevel(logging.INFO)
 	elif level == 'debug':
@@ -12,5 +19,6 @@ def makeLogger(name, level):
 		logger.setLevel(logging.ERROR)
 	elif level == 'critical':
 		logger.setLevel(logging.CRITICAL)
-
+	else:
+		logger.setLevel(0)
 	return logger
