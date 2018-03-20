d090e4bee406d361ac442c07b690b3ebf84ad96c
Adam Jermyn <adamjermyn@gmail.com>
2018-03-20 11:42:48 +0000

    Fixed bug in low-rank (below 3) identity tensor.
    Thanks to Andrei Klishin for identifying the bug.

--- TreeTensor/identityTensor.py
+++ TreeTensor/identityTensor.py
@@ -21,37 +21,39 @@ class IdentityTensor(TreeTensor):
 
         numLayers = layer(dimension)
 
-        assert rank >= 2
+        assert rank > 0
 
-        if rank == 2:
-            return ArrayTensor(np.identity(dimension))
+        if rank == 1:
+            self.addTensor(ArrayTensor(np.ones(dimension)))
+        elif rank == 2:
+            self.addTensor(ArrayTensor(np.identity(dimension)))
+        else:
+            numTensors = rank - 2
 
-        numTensors = rank - 2
+            buckets = []
 
-        buckets = []
+            # Create identity array
+            iden = np.zeros((dimension, dimension, dimension))
+            for i in range(dimension):
+                iden[i, i, i] = 1.0
 
-        # Create identity array
-        iden = np.zeros((dimension, dimension, dimension))
-        for i in range(dimension):
-            iden[i, i, i] = 1.0
+            for i in range(numTensors):
+                n = super().addTensor(ArrayTensor(iden))
+                buckets = buckets + n.buckets
 
-        for i in range(numTensors):
-            n = super().addTensor(ArrayTensor(iden))
-            buckets = buckets + n.buckets
+            while len(self.network.externalBuckets) > rank:
+                b = buckets.pop(0)
+                i = 0
+                while buckets[i].node is b.node or len(
+                        buckets[i].node.connectedNodes) > 0:
+                    i += 1
+                Link(b, buckets[i])
 
-        while len(self.network.externalBuckets) > rank:
-            b = buckets.pop(0)
-            i = 0
-            while buckets[i].node is b.node or len(
-                    buckets[i].node.connectedNodes) > 0:
-                i += 1
-            Link(b, buckets[i])
+                self.externalBuckets.remove(b)
+                self.externalBuckets.remove(buckets[i])
+                self.network.externalBuckets.remove(b)
+                self.network.externalBuckets.remove(buckets[i])
+                self.network.internalBuckets.add(b)
+                self.network.internalBuckets.add(buckets[i])
 
-            self.externalBuckets.remove(b)
-            self.externalBuckets.remove(buckets[i])
-            self.network.externalBuckets.remove(b)
-            self.network.externalBuckets.remove(buckets[i])
-            self.network.internalBuckets.add(b)
-            self.network.internalBuckets.add(buckets[i])
-
-            buckets.remove(buckets[i])
+                buckets.remove(buckets[i])
