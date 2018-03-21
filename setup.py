df924eee3c15f3d4f870108c77be34ab4b1f8d73
Adam S. Jermyn <asj42@ast.cam.ac.uk>
2018-03-21 14:59:05 +0000

    Added setup.py.

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
+
 setup(
-    name='TNR',
-    packages=find_packages(),
+	name = 'TNR',
+	version = '1.1',
+	description = 'Tensor Network Renormalization',
+	author = 'Adam S. Jermyn',
+	author_email = 'adamjermyn@gmail.com',
+	url = 'https://github.com/adamjermyn/mixer',
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Approved :: GPLv3 License',
+	'Operating System :: Microsoft :: Windows',
+	'Operating System :: POSIX',
+	'Operating System :: Unix',
+	'Operating System :: MacOS'
+	],
+	packages=find_packages(exclude=['tests','TensorNetwork_obsolete']),
+	install_requires=['numpy']
 )
+
