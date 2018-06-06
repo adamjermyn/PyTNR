020d274e10bcc5afff1f4a8c2f3d2e63caa4c2c8
Adam Jermyn <adamjermyn@gmail.com>
2018-06-06 14:45:47 +0100

    Updated config to use yaml files.

--- TNR/config.py
+++ TNR/config.py
@@ -1,3 +1,7 @@
+import yaml
+import os.path
+from pathlib import Path
+
 # Logging levels
 
 levels = {}
@@ -25,19 +29,41 @@ levels['traceMin'] = 'debug'
 levels['mergeContractor'] = 'info'
 levels['generic'] = 'info'
 
+# Run parameters
+runParams = {}
+
 # Determines the cutoff size below which matrices default to the dense SVD.
 
-svdCutoff = 1e3
+runParams['svdCutoff'] = 1e3
 
 # Determines the maximum number of attempts for the interpolative SVD.
 
-svdTries = 4
+runParams['svdTries'] = 4
 
 # Determines the maximum bond dimension for using sparse SVD. Written as a
 # fraction of the matrix rank.
 
-svdBondCutoff = 0.1
+runParams['svdBondCutoff'] = 0.1
 
 # Sets an upper bound on memory usage
 
-mem_limit = 2**33
+runParams['mem_limit'] = 2**33
+
+# Read config file if possible
+
+home = str(Path.home())
+config_path = home + '/.tnr_config'
+config_file = Path(config_path)
+if config_file.is_file():
+	data = yaml.load(open(config_path, 'r'))
+	if 'levels' in data.keys():
+		for key in data['levels'].keys():
+			levels[key] = data['levels'][key]
+	if 'runParams' in data.keys():
+		for key in data['runParams'].keys():
+			runParams[key] = data['runParams'][key]
+
+svdCutoff = int(runParams['svdCutoff'])
+svdTries = int(runParams['svdTries'])
+svdBondCutoff = float(runParams['svdBondCutoff'])
+mem_limit = int(runParams['mem_limit'])
