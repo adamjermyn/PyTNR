b0eced19b70b963ec1d449d8007d3a95bea34777
Adam Jermyn <adamjermyn@gmail.com>
2018-09-17 13:38:45 -0700

    Updated protein aggregation example.

--- TNR/Profile/protein3D.py
+++ TNR/Profile/protein3D.py
@@ -1,27 +1,24 @@
 import numpy as np
 
 from TNR.Models.protein_aggregation_3d import PA3D
-from TNR.Contractors.mergeContractor import mergeContractor
+from TNR.Contractors.mergeAllContractor import mergeContractor
 
-nX = 4
-nY = 5
-nZ = 5
+nX = 3
+nY = 3
+nZ = 3
 
-accuracy = 1e-3
+accuracy = 1e-4
 
-h = 2
-J = 2
-q = 1
+h=2.
+J=2.
+q=1.
 
 n = PA3D(nX, nY, nZ, h, J, q, accuracy)
 n = mergeContractor(
     n,
     accuracy,
-    optimize=False,
-    merge=False,
-    verbose=2,
-    mergeCut=20)
+    optimize=False)
 
 print(len(n.nodes))
 for nn in n.nodes:
-    print(np.log(nn.tensor.array))
+    print(nn.tensor.array)
