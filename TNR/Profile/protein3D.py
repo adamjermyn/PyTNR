3e34822049ea5e22bbcc5e038229a28acbc28afd
Adam Jermyn <adamjermyn@gmail.com>
2018-11-07 11:09:53 -0800

    Updated protein test.

--- TNR/Profile/protein3D.py
+++ TNR/Profile/protein3D.py
@@ -3,9 +3,9 @@ import numpy as np
 from TNR.Models.protein_aggregation_3d import PA3D
 from TNR.Contractors.mergeAllContractor import mergeContractor
 
-nX = 5
-nY = 5
-nZ = 5
+nX = 4
+nY = 4
+nZ = 4
 
 accuracy = 1e-4
 
@@ -21,4 +21,4 @@ n = mergeContractor(
 
 print(len(n.nodes))
 for nn in n.nodes:
-    print(nn.tensor.array)
+    print(nn.tensor.logNorm)
