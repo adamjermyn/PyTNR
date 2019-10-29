33c932ef729189797d547f370c52404c038da5a2
Adam Jermyn <adamjermyn@gmail.com>
2019-10-29 16:47:54 -0400

    Added merging to managedContractor

--- TNR/Contractors/managedContractor.py
+++ TNR/Contractors/managedContractor.py
@@ -29,6 +29,7 @@ def managedContractor(n, num_copies, accuracy, heuristic, optimize=True, cost_ca
         # Perform one contraction step
         try:
             logger.info('Performing contraction on network ' + str(ind) + ' with cost ' + str(costs[ind]) + '. Costs: ' + str(costs) + '.')
+            logger.info('Network has ' + str(len(net.nodes)) + ' nodes.')
             q, n1, n2 = heuristic(net)
             n3 = net.mergeNodes(n1, n2)
 
@@ -37,23 +38,34 @@ def managedContractor(n, num_copies, accuracy, heuristic, optimize=True, cost_ca
             if optimize:
                 n3.tensor.optimize()
 
+            logger.info('Merging nodes...')
+            nn = n3
+            if hasattr(
+                    nn.tensor, 'compressedSize'):
+                done2 = False
+                while not done2:
+                    merged = net.mergeClosestLinks(
+                        n3, compress=True, accuracy=accuracy)
+                    if merged is not None:
+                        nn.eliminateLoops()
+                        merged.eliminateLoops()
+                        if optimize:
+                            nn.tensor.optimize()
+                            merged.tensor.optimize()
+                    else:
+                        done2 = True
+
+            logger.info('Merging complete.')
+
             costs[ind] = net.compressedSize
 
             if len(net.internalBuckets) == 0:
                 done = True
+
+
+
         except KeyboardInterrupt:
             exit()
-        except:
-            e = sys.exc_info()[0]
-            logger.info(str(e))
-            logger.info('Failed to contract network ' + str(ind) + '.')
-            logger.info('Replacing that with a clone of the next best network.')
-            # Clone the current best network in place of the failed one
-            del networks[ind]
-            del costs[ind]
-            ind = costs.index(min(costs))
-            networks.append(deepcopy(networks[ind]))
-            costs.append(costs[ind])
 
         if cost_cap is not None:
             for i in range(num_copies):
