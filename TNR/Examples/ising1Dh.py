Fixed logZ calculation in examples

TNR/Examples/ising1Dh.py
@@ -15,7 +15,9 @@ def ising1DFreeEnergy(nX, h, J, accuracy):
         optimize=False,
         merge=False,
         plot=False)
-    return n.array[1] / nX
+
+    arr, log_arr, bdict = n.array
+    return (np.log(arr) + log_arr) / nX
 
 
 for h in [-2, -1, -0.5, 0, 0.5, 1, 2]:

TNR/Examples/ising2DJopen.py
@@ -20,7 +20,9 @@ def ising2DFreeEnergy(nX, nY, h, J, accuracy):
         heuristic,
         optimize=True,
         cost_cap = 1e9)
-    return n.array[1] / (nX * nY)
+
+    arr, log_arr, bdict = n.array
+    return (np.log(arr) + log_arr) / (nX * nY)
 
 
 accuracy = 1e-3

TNR/Examples/ising2Dh.py
@@ -15,8 +15,9 @@ def ising2DFreeEnergy(nX, nY, h, J, accuracy):
         optimize=True,
         merge=False,
         plot=False)
-    arr = n.array
-    return n.array[1] / (nX * nY)
+
+    arr, log_arr, bdict = n.array
+    return (np.log(arr) + log_arr) / (nX * nY)
 
 
 for h in [-2, -1, -0.5, 0, 0.5, 1, 2]:

TNR/Examples/isingGlass.py
@@ -19,8 +19,9 @@ def isingFreeEnergy(nX, J, k, accuracy):
         optimize=True,
         merge=False,
         plot=False)
-    return n.array[1] / (nX)
 
+    arr, log_arr, bdict = n.array
+    return (np.log(arr) + log_arr) / nX
 
 J = 1
 accuracy = 1e-3