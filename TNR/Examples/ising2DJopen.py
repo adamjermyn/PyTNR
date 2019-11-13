Fixed normalization

TNR/Examples/ising2DJopen.py
@@ -22,7 +22,7 @@ def ising2DFreeEnergy(nX, nY, h, J, accuracy):
         cost_cap = 1e9)
 
     arr, log_arr, bdict = n.array
-    return (np.log(np.abs(arr)) + log_arr) / nX
+    return (np.log(np.abs(arr)) + log_arr) / (nX * nY)
 
 
 accuracy = 1e-3