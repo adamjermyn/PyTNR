00eff725812f20fe938c04e434a11ed25249ee0d
Adam Jermyn <adamjermyn@gmail.com>
2020-02-17 14:24:32 -0500

    Refactored loop_opt example.

--- TNR/Examples/ising2DJ_loop_opt.py
+++ TNR/Examples/ising2DJ_loop_opt.py
@@ -1,10 +1,10 @@
 import numpy as np
 import time
-import sys
 
-from TNR.Models.isingModel import IsingModel2D, exactIsing2D
-from TNR.Contractors.mergeContractor import mergeContractor
+from TNR.Models.isingModel import IsingModel2Ddisordered
+from TNR.Contractors.contractor import replicaContractor
 from TNR.Contractors.heuristics import loopHeuristic as heuristic
+from TNR.Actions.loop_svd_elim import loop_svd_elim as eliminateLoops
 
 from TNR.Utilities.logger import makeLogger
 from TNR import config
@@ -12,39 +12,43 @@ logger = makeLogger(__name__, config.levels['generic'])
 
 
 def ising2DFreeEnergy(nX, nY, h, J, accuracy):
-    n = IsingModel2D(nX, nY, h, J, accuracy)
-    n = mergeContractor(
-        n,
-        accuracy,
-        heuristic,
-        optimize=False,
-        merge=False,
-        plot=False)
-    return n.array[1] / (nX * nY)
+    n = IsingModel2Ddisordered(nX, nY, h, J, accuracy)
+
+    c = replicaContractor(n, 5, 1e6)
+    done = False
+    while not done:
+        node, done, ind, replaced = c.take_step(heuristic)
+        if not replaced:
+            eliminateLoops(node.tensor, False)
+            c.optimize(new_node)
+    n = c.replicas[ind].network
+
+    arr, log_arr, bdict = n.array
+    return (np.log(np.abs(arr)) + log_arr) / (nX * nY)
 
 
-J = float(sys.argv[1])
-
-h = 0
-accuracy = 1e-3
-size = [(7,7)]
-
+h = 1
+J = 1
+accuracy = 1e-6
+size = [(2, 2), (2, 3), (2, 4), (3, 3), (2, 5), (3, 4), (4, 4), (3, 6), (4, 5), (3, 7), (3, 8), (5, 5), (3, 9),
+        (4, 7), (5, 6), (4, 8), (5, 7), (6, 6), (6, 7), (7, 7), (7, 8), (8, 8), (8, 9)]
 res = []
 
 for s in size:
-    logger.info(
-        'Examining system of size ' +
-        str(s) +
-        ' and J = ' +
-        str(J) +
-        '.')
-    start = time.clock()
-    f = ising2DFreeEnergy(s[0], s[1], h, J, accuracy)
-    end = time.clock()
-    res.append((s[0] * s[1], f, f - exactIsing2D(J), end - start))
+    for _ in range(3):
+        logger.info(
+            'Examining system of size ' +
+            str(s) +
+            ' and J = ' +
+            str(J) +
+            '.')
+        start = time.clock()
+        f = ising2DFreeEnergy(s[0], s[1], h, J, accuracy)
+        end = time.clock()
+        res.append((s[0] * s[1], f, end - start))
 
 res = np.array(res)
 
 print(res)
 
-np.savetxt('ising2DJ_J=' + str(J) + '.dat', res)
+np.savetxt('ising2D_disordered.dat', res)
